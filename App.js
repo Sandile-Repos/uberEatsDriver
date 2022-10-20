8db88d157a95ab330023164b291904247d806968
Sandile-Repos <sandile.developer@gmail.com>
2022-10-20 17:31:32 +0200

    add amplify backend and ProfileScreen

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+import AuthContextProvider from "./src/contexts/AuthContext";
 
-export default function App() {
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+function App() {
   return (
     <NavigationContainer>
-      <Navigation />
-
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OrderScreen from "../screens/OrderScreen";
 import OrderDelivery from "../screens/OrderDelivery";
-
+import ProfileScreen from "../screens/ProfileScreen";
+import { useAuthContext } from "../contexts/AuthContext";
 const Stack = createNativeStackNavigator();
-const Navigation = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="OrdersScreen" component={OrderScreen} />
-    <Stack.Screen name="OrdersDeliveryScreen" component={OrderDelivery} />
-  </Stack.Navigator>
-);
+const Navigation = () => {
+  const { dbCourier } = useAuthContext();
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {dbCourier ? (
+        <>
+          <Stack.Screen name="OrdersScreen" component={OrderScreen} />
+          <Stack.Screen name="OrdersDeliveryScreen" component={OrderDelivery} />
+        </>
+      ) : (
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      )}
+    </Stack.Navigator>
+  );
+};
 export default Navigation;
