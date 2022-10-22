7cea61095924ed416c1f250e600ca4f151708dac
Sandile-Repos <sandile.developer@gmail.com>
2022-10-22 18:34:13 +0200

    add loading state to prevent profile screen route if user is undefine when logged

--- src/contexts/AuthContext/index.js
+++ src/contexts/AuthContext/index.js
@@ -7,6 +7,7 @@ const AuthContext = createContext({});
 const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(null);
   const [dbCourier, setDbCourier] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const sub = authUser?.attributes?.sub;
 
@@ -17,16 +18,21 @@ const AuthContextProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    //setting database Courier when component mounts
+    if (!sub) {
+      return;
+    }
     DataStore.query(Courier, (courier) => courier.sub("eq", sub))
       .then((couriers) => {
         setDbCourier(couriers[0]);
+        setLoading(false);
       })
       .catch((error) => console.log(error));
   }, [sub]);
 
   return (
-    <AuthContext.Provider value={{ authUser, dbCourier, sub, setDbCourier }}>
+    <AuthContext.Provider
+      value={{ authUser, dbCourier, sub, setDbCourier, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
